0f2bb598ab6a52d3c637bf128e0567cae5d6402b
Adam Brinnig <adamjast@mac.com>
2016-11-20 12:44:06 -0500

    last clean

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -18,10 +18,10 @@ $('#side-menu > li:nth-child(-n+8)').hide(); // kill dashboard plus everything e
 
 // add all feature links
 // $('#side-menu').append('<li><a href="congratulations.html"><i class="fa fa-female" aria-hidden="true"></i>Congratulations!</a></li>');
-$('#side-menu').append('<li><a href="guider.html"><i class="fa fa-dashboard fa-fw"></i>Guider</a></li>'); // guider
-$('#side-menu').append('<li><a href="product_recommender.html"><i class="fa fa-dashboard fa-fw"></i>Product Recommender</a></li>'); // product recommender
+$('#side-menu').append('<li><a href="guider.html"><i class="fa fa-dashboard fa-fw"></i>Guide</a></li>'); // guider
+$('#side-menu').append('<li><a href="product_recommender.html"><i class="fa fa-dashboard fa-fw"></i>Discover Products</a></li>'); // product recommender
 $('#side-menu').append('<li><a href="calendar.html"><i class="fa fa-dashboard fa-fw"></i>Calendar</a></li>'); // calendar
-$('#side-menu').append('<li><a href="purchaser.html"><i class="fa fa-dashboard fa-fw"></i>Purchaser</a></li>'); // purchaser
+$('#side-menu').append('<li><a href="purchaser.html"><i class="fa fa-dashboard fa-fw"></i>Samples</a></li>'); // purchaser
 
 
 // change the name of the project logo
@@ -29,7 +29,8 @@ var logoTitle = "Monarch";
 
 // make root page 'guide' and change logo
 // .navbar-brand
-$('.navbar-brand').html('<a class="navbar-brand" href="index.html">' + logoTitle + '</a>'); // purchaser
+$('.navbar-brand').html('<a class="navbar-brand" href="index.html">' + logoTitle + '</a>'); // brand
+// $('.navbar-brand').html('<a class="navbar-brand" href="index.html"><img src="../logo_1.png" ></a>');
 // code...
 
 
